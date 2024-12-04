f8066517d84d4bf724a59762d49def99b81ef8f3
Gareth Hind <garethjhind@gmail.com>
2024-12-04 21:37:16 +1100

    Feature: Added FixturePage.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </Provider>
+  </QueryClientProvider>
 );
