2aea8eaadd496bf101192221c90998a7633d7144
Gareth Hind <garethjhind@gmail.com>
2025-02-12 21:09:22 +1100

    Refactor: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,46 @@
-import React from "react"; // useEffect // lazy, // Suspense,
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  // Navigate,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Context
 import { AuthProvider } from "../context/authContext";
 
+// Layout
 import Navbar from "../layout/Navbar";
-// Pages
-import Login from "../pages/Login";
-import Register from "../pages/Register";
+
+// Pages & Components
 import Tipping from "../pages/Tipping";
 import PrivateRoute from "../pages/PrivateRoute";
+import LoginForm from "../feature/LoginForm";
+import RegisterForm from "../feature/RegisterForm";
 
 import "../styles/index.scss";
+
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <div>
+    <Navbar />
+    {children}
+  </div>
+);
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
-      <Navbar />
       <Router>
-        <Routes>
-          <Route path="/" element={<Tipping />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/tipping"
-            element={
-              <PrivateRoute>
-                <Tipping />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Tipping />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/register" element={<RegisterForm />} />
+            <Route
+              path="/tipping"
+              element={
+                <PrivateRoute>
+                  <Tipping />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Layout>
       </Router>
     </AuthProvider>
   );
